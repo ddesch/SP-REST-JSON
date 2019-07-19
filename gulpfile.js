c442d398a810642e2b6db9c6a4dcc89e8001b5ab
ddesch <danieldesch@gmx.de>
2019-07-19 16:35:22 +0200

    Added gulp file and moved source to src folder

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const { src, dest } = require('gulp');
+const args = require('gulp-args');
+const replace = require('gulp-replace');
+const zip = require('gulp-zip');
+const publishVersion = args.publishVersion;
+ 
+exports.default = function(done) {
+  if(publishVersion !== undefined) {
+    src(['./src/manifest.json', './src/SPRESTJSON.js'])
+    .pipe(replace(/(\d+)\.(\d+)\.(\d+)/, publishVersion))
+    .pipe(dest('./src/'));
+  }
+  zipping();
+  done();
+}
+
+zipping = function() {
+    return src(['./src/**', 'LICENSE'])
+        .pipe(zip('SP-REST-JSON_' + publishVersion + '.zip'))
+        .pipe(dest('dist'));
+};
