23e5b852cc8cee9a9286ed4134d35e6a932d516d
danieldesch@gmx.de <danieldesch@gmx.de>
2024-07-17 18:07:09 +0200

    Revert to icons\128 folder

--- createFolderAndZip.ps1
+++ createFolderAndZip.ps1
@@ -18,4 +18,5 @@ Copy-Item -Path .\src\* -Destination ".\dist\SP-REST-JSON_$($version)\" -Recurse
 # Add files to the zip file
 # (Existing files in the zip file with the same name are replaced)
 # Compress-Archive -Path ".\src\*" -Update -DestinationPath ".\dist\SP-REST-JSON_$($version).zip"
-Compress-Archive -Path ".\dist\SP-REST-JSON_$($version)\*" -Update -DestinationPath ".\dist\SP-REST-JSON_$($version).zip"
+# Compress-Archive -Path ".\dist\SP-REST-JSON_$($version)\*" -Update -DestinationPath ".\dist\SP-REST-JSON_$($version).zip"
+Write-Host "Mozilla mag die gezippte Version nicht --> SELBST MANUELL ZIPPEN"
